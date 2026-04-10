4d50c6ffd583f67deb28e551625c309d667f8b60
Garrett Lander <garrett.jlander@gmail.com>
2026-04-10 10:43:47 +0200

    ef core lecture

---
+++ 25-asp-dot-net/02-db-and-auth/01-EFCore-Lecture/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using BlogApi.Endpoints;
+using BlogApi.Services;
+using BlogApi.Infrastructure;
+using BlogApi.Infrastructure.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IPostService, PostService>();
+builder.Services.AddScoped<DbSeeder>();
+// builder.Services.AddSingleton<IUserService, InMemoryUserService>();
+// builder.Services.AddSingleton<IPostService, InMemoryPostService>();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+    using (var scope = app.Services.CreateScope())
+    {
+        var seeder = scope.ServiceProvider.GetRequiredService<DbSeeder>();
+        await seeder.SeedAsync();
+    }
+}
+
+app.MapUserEndpoints();
+app.MapPostEndpoints();
+
+app.Run();
